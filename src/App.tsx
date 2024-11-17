e17fd06bdb09f676a9aef1ae82c09f5aaf10f9dc
hissoune <khalidhissoune962@gmail.com>
2024-11-17 17:27:56 +0100

    Add : wrapping the home component with the context that provides the socket

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,19 @@ import HomePage from "./pages/HomePage.tsx";
 import {Route, Routes} from "react-router-dom";
 import Login from "./auth/LoginAuth.tsx";
 import {NotfoundPage} from "./pages/NotfoundPage.tsx";
+import SocketProvider from './helper/SocketContext.tsx';
 function App() {
 
   return (
     <>
         <Routes>
-            <Route path='/home' element={<HomePage/>}/>
+            <Route path='/home' element={
+                    <SocketProvider>
+                      <HomePage/>
+                    </SocketProvider>
+
+              
+              }/>
             <Route path='/' element={<Login/>}/>
             <Route path='*' element={<NotfoundPage/>}/>
         </Routes>
