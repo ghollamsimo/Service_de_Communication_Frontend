280db333efe5ec6f7651bbaa084554fde7aa03fd
hissoune <khalidhissoune962@gmail.com>
2024-11-18 17:51:16 +0100

    Add : adding the channel slice in the store

--- src/redux/Store.ts
+++ src/redux/Store.ts
@@ -1,11 +1,13 @@
 import {configureStore} from "@reduxjs/toolkit";
 import authSlice from "./slices/AuthSlice.ts";
 import friendSlice from "./slices/FriendSlice.ts";
+import channelSlice from './slices/ChannelSlice';
 
 export const store = configureStore({
     reducer: {
         auth: authSlice,
-        friend: friendSlice
+        friend: friendSlice,
+        channel:channelSlice
     }
 })
 export type RootState = ReturnType<typeof store.getState>;
